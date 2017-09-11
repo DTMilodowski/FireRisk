27a64f218540003565974697873cc11317741cf0
David Milodowski <d.t.milodowski@ed.ac.uk>
2017-09-11 12:22:46 +0100

    documentation

--- src/Mark5/calculate_Mark5.py
+++ src/Mark5/calculate_Mark5.py
@@ -21,7 +21,7 @@
 # - wind velocity, U
 # - precipitation, P
 # - number of days since previous rainfall event, d
-# - Keetch-Byram drought index [Keetch & Byram, 1968], I
+# - Keetch-Byram drought index [Keetch & Byram, 1968], KB
 #
 # By including the K-B drought index, the Mark5 approach does attempt to
 # capture some of the dynamics of soil and litter moisture on fire risk.
@@ -33,12 +33,14 @@ import numpy as np
 # on a lookup table, with the index updated at each iteration by removing net
 # precipitation and incrementing the index to account for drying.
 def calculate_Keetch_Byram_drought_index(maxT, P24):
-    
+
+    return KB
+
 # Calculation of the Mark5 index based on weather and K-B drought index
 def calculate_Mark5(H,T,U,P,d,I):
 
     # First calculate drought factor
-    DF = (0.191*(I+104)*(d+1)**1.5)/(3.52*(d+1)**1.5 + P-1)
+    DF = (0.191*(KB+104)*(d+1)**1.5)/(3.52*(d+1)**1.5 + P-1)
 
     # Now use this to calculate fire risk
     F = 2.0*exp(-0.450 + 0.987*np.log(DF) - 0.0345*H + 0.0338*T + 0.0234*U)
