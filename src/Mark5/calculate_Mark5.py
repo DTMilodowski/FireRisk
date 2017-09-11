ecad1cf3d03974b83de50c075790c8006254d1e6
David Milodowski <d.t.milodowski@ed.ac.uk>
2017-09-11 12:03:51 +0100

    Mark5 fire index

--- src/Mark5/calculate_Mark5.py
+++ src/Mark5/calculate_Mark5.py
@@ -8,5 +8,27 @@
 # danger rating system. This is a relatively simple fire risk metric
 # predominately determined by meteorological conditions. The original equations
 # were published by Noble et al., Aust. J. Ecol., 1980.
+#
+# Note that the Mark5 index does not account for factors such as diversity in 
+# the abundance or characteristics of fuel loads, or topographic slope, and is
+# based on an empirical formulation of fire risk from Australia
+#
+# Meteorological variables required:
+# - relative humidity, H
+# - temperature, T
+# - wind velocity, U
+# - precipitation, P
+# - number of days since previous rainfall event, d
+# - Keetch-Byram drought index [Keetch & Byram, 1960], I
 #==============================================================================
+import numpy as np
+
+
+def calculate_Mark5(H,T,U,P,d,I):
+
+    # First calculate drought factor
+    DF = (0.191*(I+104)*(d+1)**1.5)/(3.52*(d+1)**1.5 + P-1)
+
+    # Now use this to calculate fire risk
+    F = 2.0*exp(-0.450 + 0.987*np.log(DF) - 0.0345*H + 0.0338*T + 0.0234*U)
 
