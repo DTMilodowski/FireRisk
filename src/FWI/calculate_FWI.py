ae5c1d45d73167dde97e4f16656ec9157e2342d9
David Milodowski <d.t.milodowski@ed.ac.uk>
2017-09-11 10:20:28 +0100

    new script to calculate FWI fire risk index

---
+++ src/FWI/calculate_FWI.py
@@ -0,0 +1,13 @@
+#==============================================================================
+# calculate_FWI.py
+#
+# David T. Milodowski
+# September 2017
+#
+# This module estimates fire risk based on the Canadian Fire Weather Index (FWI)
+# danger rating system. This system combines meteorological conditions with an
+# estimate of fuel moisture levels to determine an index of overall fire risk.
+# For a detailed overview of the system, please see Stocks et al., The Forestry
+# Chronicle, 1989
+#==============================================================================
+
