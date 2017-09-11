65af4575356cd4d75b84bf7c1f9c2883e1cfab19
David Milodowski <d.t.milodowski@ed.ac.uk>
2017-09-11 18:24:55 +0100

    updating docs

--- src/FWI/calculate_FWI.py
+++ src/FWI/calculate_FWI.py
@@ -9,5 +9,24 @@
 # estimate of fuel moisture levels to determine an index of overall fire risk.
 # For a detailed overview of the system, please see Stocks et al., The Forestry
 # Chronicle, 1989
+#
+# The FWI is a "3-tiered" system comprising a series of indices that combine
+# together to provide an integrated fire index system. It is more complex than
+# the Australian McArthur system in the way it treats fuel moisture -
+# separating out three different classes with different response times - but
+# still lacks a treatment of fuel load abundance and diversity.
+#
+# The model components are summarised in the flowchart below
+#
+#        Level 1                       Level 2                   Level 3
+#
+# Fine Fuel Moisture Code ----> Initial Spread Index _
+#         FFMC                          ISI           \
+#                                                      \
+#   Duff Moisture Code ___                              ---> Fire Weather Index
+#         DMC              \                           /          FWI
+#                           -----> Build-up Index ____/
+#     Drought Code ________/            BUI
+#          DC
 #==============================================================================
 
