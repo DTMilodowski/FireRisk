56896a616c6e8285235ae31536a069d29cf1a83c
David Milodowski <d.t.milodowski@ed.ac.uk>
2017-09-11 10:16:54 +0100

    new script to calculate McArthur Mark5 fire risk

---
+++ src/Mark5/calculate_Mark5.py
@@ -0,0 +1,12 @@
+#==============================================================================
+# calculate_Mark5.py
+#
+# David T. Milodowski
+# September 2017
+#
+# This module estimates fire risk based on the Australian McArthur (Mark5) fire
+# danger rating system. This is a relatively simple fire risk metric
+# predominately determined by meteorological conditions. The original equations
+# were published by Noble et al., Aust. J. Ecol., 1980.
+#==============================================================================
+
