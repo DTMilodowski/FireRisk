8ae737a1371f9145159c553e26a5c6796ac2d6a0
David Milodowski <d.t.milodowski@ed.ac.uk>
2017-09-12 17:05:52 +0100

    bringing repo up to date

--- src/Mark5/calculate_Mark5.py
+++ src/Mark5/calculate_Mark5.py
@@ -45,6 +45,7 @@ def calculate_Keetch_Byram_drought_index(Q0, maxT, P24, MAP, dt_days = 1, interc
     
     # (3) update moisture deficiency
     Q = Q0 + dQ_pttn + dQ_drying
+
     if Q < 0:
         Q=0
 
