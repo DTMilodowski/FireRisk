988d335a44f93a7480c63aadaf8f004f21e0af63
David Milodowski <d.t.milodowski@ed.ac.uk>
2017-09-20 11:30:26 +0100

    test driving the FWI

---
+++ src/FWI/FWI_driver.py
@@ -0,0 +1,16 @@
+#==============================================================================
+# FWI_driver.py
+#
+# David T. Milodowski
+# September 2017
+#
+# This is a simple driver function to test out the FWI module. It loads in the
+# meteorological data, spins up the moisture codes and then iterates through
+# the timesteps. At a later point, this will be incorporated into a dedicated
+# object
+#==============================================================================
+# standard libraries
+import numpy
+from matplotlib import pyplot as plt
+# own libraries
+import 
