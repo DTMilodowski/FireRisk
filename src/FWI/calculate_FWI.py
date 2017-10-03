fixed bugs in fwi calculation

src/FWI/calculate_FWI.py
@@ -103,10 +103,10 @@ def calculate_FFMC_array(H,T,W,P,FFMC0=-9999):
     #     Wetting and drying rates assumed to be exponential, with rate 
     #     constants a function of temperature, windspeed and relative humidity
     #     - drying
-    k0_d = 0.424*(1-(H/100)**1.7) + 0.0694*W**0.5*(1-(H/100)**8) # equation 4
+    k0_d = 0.424*(1.-(H/100.)**1.7) + 0.0694*W**0.5*(1.-(H/100.)**8) # equation 4
     kd = k0_d * 0.581*np.exp(0.00365*T) # equation 6
     #     - wetting
-    k0_w = 0.424*(1-((100-H)/100)**1.7) + 0.0694*W**0.5*(1-((100-H)/100)**8) # equation 5
+    k0_w = 0.424*(1.-((100-H)/100.)**1.7) + 0.0694*W**0.5*(1.-((100-H)/100.)**8) # equation 5
     kw = k0_w * 0.581*np.exp(0.00365*T) # equation 6
 
     # (2) Calculate equilibrium moisture contents for fine fuel load. Units are