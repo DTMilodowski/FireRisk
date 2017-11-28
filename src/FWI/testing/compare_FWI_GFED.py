c478eb35711248b9cdb803f3ca442cbc8fd21258
David Milodowski <d.t.milodowski@ed.ac.uk>
2017-11-28 17:06:57 +0000

    ready to go for tomorrow

--- src/FWI/testing/compare_FWI_GFED.py
+++ src/FWI/testing/compare_FWI_GFED.py
@@ -5,3 +5,32 @@
 #-------------------------------------------------------------------------------
 # This routine tests the FWI components against GFED burned area data
 #===============================================================================
+import numpy as np
+from matplotlib import pyplot as plt
+from matplotlib import rcParams
+from mpl_toolkits.basemap import Basemap
+# Set up some basiic parameters for the plots
+rcParams['font.family'] = 'sans-serif'
+rcParams['font.sans-serif'] = ['arial']
+rcParams['font.size'] = 8
+rcParams['legend.numpoints'] = 1
+axis_size = rcParams['font.size']+2
+
+import sys
+
+# Get perceptually uniform colourmaps
+sys.path.append('/home/dmilodow/DataStore_DTM/FOREST2020/EOdata/EO_data_processing/src/plot_EO_data/colormap/')
+import colormaps as cmaps
+plt.register_cmap(name='viridis', cmap=cmaps.viridis)
+plt.register_cmap(name='inferno', cmap=cmaps.inferno)
+plt.set_cmap(cmaps.inferno) # set inferno as default - a good fiery one :-)
+
+sys.path.append('/home/dmilodow/DataStore_DTM/FOREST2020/FireRisk/src/FWI/')
+import calculate_FWI as fwi
+import plot_FWI as fwi_p
+
+sys.path.append('/exports/csce/datastore/geos/users/dmilodow/FOREST2020/EOdata/EO_data_processing/src/meteorology')
+import load_ERAinterim as era
+
+sys.path.append('/exports/csce/datastore/geos/users/dmilodow/FOREST2020/EOdata/EO_data_processing/src/fire/')
+import load_GFED as GFED
