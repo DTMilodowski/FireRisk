e4439ae240971d42fb8fb0975315cf9434c8e78c
David Milodowski <d.t.milodowski@ed.ac.uk>
2018-07-25 17:01:43 +0100

    plotting cumulative pixels

--- src/FWI/testing/plot_fire_statistics.py
+++ src/FWI/testing/plot_fire_statistics.py
@@ -1,20 +1,24 @@
 import numpy as np
 import os
 import sys
-sys.path.append('/exports/csce/datastore/geos/users/dmilodow/FOREST2020/EOdata/EO_data_processing/src/')
-sys.path.append('/home/dmilodow/DataStore_DTM/FOREST2020/FireRisk/src/generic/')
-sys.path.append('/exports/csce/datastore/geos/users/dmilodow/FOREST2020/EOdata/EO_data_processing/src/meteorology')
-import load_ERAinterim as era
-import load_MODIS as modis
-import load_ESA_CCI_lc as cci
-import clip_raster as clip
-import resample_raster as rs
-import data_io as io
-from scipy import ndimage
+import datetime as dt
 
 from matplotlib import pyplot as plt
 import seaborn as sns
 sns.set()
 
+# simple plot to show how cumulative number of fire affected pixels increases
+# through time. This allows us to infer whether the fire record is likely to
+# adequately capture the spatial distribution of fire-affected area (saturation)
+# or not (continued increase). This is important for understanding the extent
+# to which the regional fire hazard is adequately captured by the data.
 
-
+def plot_cumulative_fire_affected_pixels(fignum,figname,month,pix):
+    fig = plt.figure(fignum, facecolor='White',figsize=[4,4])
+    ax = plt.subplot2grid((1,1),(0,0))
+    ax.plot(month.astype(dt.datetime),pix/1000.)
+    ax.set_ylabel('Cumulative fire affected pixels / 10$^3$')
+    plt.tight_layout()
+    plt.savefig(figname)
+    plt.show()
+    return 0
